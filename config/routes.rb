75a9f0ceb6d6daa9a7cf37ac437306fd62369426
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-06-19 20:20:04 +0100

    Configure initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :activities
-  resources :exercises
-  resources :users
+
+  root 'users#welcome'
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:welcome, :new, :create, :show]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
