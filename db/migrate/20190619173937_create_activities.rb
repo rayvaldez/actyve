b67dad35ba303c591dc8d9a81b2ef99f7c031471
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-06-19 18:43:19 +0100

    Create Activity model

---
+++ db/migrate/20190619173937_create_activities.rb
@@ -0,0 +1,16 @@
+class CreateActivities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :activities do |t|
+      t.string :location
+      t.integer :distance
+      t.integer :hour
+      t.integer :minute
+      t.integer :calories
+      t.string :comment
+      t.references :user, foreign_key: true
+      t.references :exercise, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
