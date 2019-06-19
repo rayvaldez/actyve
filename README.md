97a4b15dbc66c3b5efbefd7fd3368311198af9d5
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-06-19 19:55:44 +0100

    Add User associations

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ACTYVE is an activity tracking app built with Ruby on Rails.
 
-[!ACTYVE](app/assets/images/actyve.png)
+[![ACTYVE](app/assets/images/actyve.png)
 
 ## Description
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :activities
+  has_many :exercises, through: :activities
+
+  has_secure_password
+
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 end
