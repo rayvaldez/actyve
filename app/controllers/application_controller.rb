16f3b76681a4b2f2221f695321ce0ada84baabf9
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-06-21 17:25:03 +0100

    Add helper methods for User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :is_logged_in?, :authenticate_user!
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def is_logged_in?
+    !!current_user
+  end
+
+  def authenticate_user!
+    redirect_to new_session_path if !is_logged_in?
+  end
 end
