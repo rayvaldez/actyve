688c98ebdba72b2115d58466439abf01fb0b4d55
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-06-22 17:11:43 +0100

    Add Activity model validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,8 @@
 class Activity < ApplicationRecord
   belongs_to :user
   belongs_to :exercise
+
+  validates :minute, numericality: { less_than_or_equal_to: 60}
+  validates :distance, :hour, :calories, numericality: { only_integer: true}
+
 end
