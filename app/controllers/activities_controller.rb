c3d512d917f670ca25f0a3f56a867cf3d1340118
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-06-22 17:02:15 +0100

    Add new/create/show view for new activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,33 @@
 class ActivitiesController < ApplicationController
 
-  def new
+  def index
     @activities = Activity.all
+  end
+
+  def new
+    @user = current_user
+    @exercise = Exercise.find_by_id(params[:exercise_id])
+    @activity = Activity.new
+  end
+
+  def create
+    @exercise = Exercise.find_by_id(params[:exercise_id])
+    @activity = current_user.activities.new(activity_params)
+
+    if @activity.save!
+      redirect_to exercise_activity_path(@exercise, @activity)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @activity = Activity.find_by(id: params[:id])
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:location, :hour, :minute, :calories, :comment, :distance, :exercise_id, :user_id)
   end
 end
